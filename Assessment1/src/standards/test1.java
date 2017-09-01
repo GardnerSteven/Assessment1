0d9a8320987e98ff64b7902ecfd601061e6b4d53
GardnerSteven <31359320+GardnerSteven@users.noreply.github.com>
2017-09-01 08:30:14 -0500

    update

---
+++ Assessment1/src/standards/test1.java
@@ -0,0 +1,14 @@
+package standards;
+
+import javax.swing.JOptionPane;
+
+public class test1 
+{
+
+	public static void main(String[] args) 
+	{
+		JOptionPane.showMessageDialog(null,"says something funny");
+
+	}
+
+}
---
+++ Assessment1/src/standards/test3.java
@@ -0,0 +1,14 @@
+package standards;
+
+import javax.swing.JOptionPane;
+
+public class test3 
+{
+
+	public static void main(String[] args) 
+	{
+	JOptionPane.showMessageDialog(null, "says something funny");	
+    //System.out.println("Mr. Brosius is a teacher");.//
+	}
+
+}
